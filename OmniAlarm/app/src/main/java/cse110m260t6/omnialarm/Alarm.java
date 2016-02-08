4c3eddd4bf1eaa56b2e6dcc909d59985d121b201
djing123 <djing1994@gmail.com>
2016-02-08 11:42:28 -0800

    add class

---
+++ OmniAlarm/app/src/main/java/cse110m260t6/omnialarm/Alarm.java
@@ -0,0 +1,9 @@
+package cse110m260t6.omnialarm;
+
+/**
+ * Created by dadongjing on 2/8/16.
+ */
+public class Alarm {
+
+
+}
---
+++ OmniAlarm/app/src/main/java/cse110m260t6/omnialarm/Database.java
@@ -0,0 +1,8 @@
+package cse110m260t6.omnialarm;
+
+/**
+ * Created by dadongjing on 2/8/16.
+ */
+public class Database {
+    
+}
