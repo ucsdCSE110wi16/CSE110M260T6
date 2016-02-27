df67e4e453271aa72ba0655d83bfc981559603db
djing123 <djing1994@gmail.com>
2016-02-26 20:42:49 -0800

    delete floating acting bar

--- OmniAlarm/app/src/main/java/cse110m260t6/omnialarm/main_ac.java
+++ OmniAlarm/app/src/main/java/cse110m260t6/omnialarm/main_ac.java
@@ -24,14 +24,7 @@ public class main_ac extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         
 
 
