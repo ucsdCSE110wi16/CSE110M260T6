52a2bad959bd1ba75b33f57b6e4071c9bfd1fc77
djing123 <djing1994@gmail.com>
2016-02-26 23:06:52 -0800

    set time input boundary

--- OmniAlarm/app/src/main/java/cse110m260t6/omnialarm/addAlarm.java
+++ OmniAlarm/app/src/main/java/cse110m260t6/omnialarm/addAlarm.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Calendar;
 
@@ -17,7 +18,7 @@ public class addAlarm extends AppCompatActivity{
         Button choose_time;
         EditText T1;
         EditText T2;
-
+        TextView T3;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -38,26 +39,43 @@ public class addAlarm extends AppCompatActivity{
                     //get the hour and minute user specified
                     T1 = (EditText)findViewById(R.id.hour);
                     T2 = (EditText)findViewById(R.id.minute);
+                    T3 = (TextView)findViewById(R.id.time_not_valid);
                     String hour = T1.getText().toString();
                     String minute = T2.getText().toString();
 
-                    //set up the calendar object of the specified hour and minute
-                    Calendar myCa = Calendar.getInstance();
-                    myCa.set(Calendar.HOUR_OF_DAY,Integer.parseInt(hour));
-                    myCa.set(Calendar.MINUTE,Integer.parseInt(minute));
 
-                    //get the temp alarm from database
-                    Alarm myAl = Database.getTempAlarm();
+                    if(!hour.equals("")  && !minute.equals("")){
+                        int hour_int = Integer.parseInt(hour);
+                        int minute_int = Integer.parseInt(minute);
+                        if(hour_int < 24 && hour_int > 0 && minute_int > 0 && minute_int < 60){
+                            //set up the calendar object of the specified hour and minute
+                            Calendar myCa = Calendar.getInstance();
+                            myCa.set(Calendar.HOUR_OF_DAY,Integer.parseInt(hour));
+                            myCa.set(Calendar.MINUTE,Integer.parseInt(minute));
 
-                    //update the alarm time
-                    myAl.setTime(myCa);
+                            //get the temp alarm from database
+                            Alarm myAl = Database.getTempAlarm();
 
-                    //update the temp alarm in the database
-                    Database.updateTemp(myAl);
+                            //update the alarm time
+                            myAl.setTime(myCa);
 
-                    //jump to next page that prompt user to enter ring tone
-                    Intent jumpActivity = new Intent(v.getContext(), cse110m260t6.omnialarm.ringTone.class);
-                    startActivityForResult(jumpActivity, 0);
+                            //update the temp alarm in the database
+                            Database.updateTemp(myAl);
+
+                            //jump to next page that prompt user to enter ring tone
+                            Intent jumpActivity = new Intent(v.getContext(), cse110m260t6.omnialarm.ringTone.class);
+                            startActivity(jumpActivity);
+                        }
+                        else{
+                            String errorMes = new String("Time NOT VALID!");
+                            T3.setText(errorMes);
+                        }
+                    }
+                    else{
+                        String errorMessage = new String("Please INPUT TIME");
+                        T3.setText(errorMessage);
+                    }
+
                 }
             });
 
